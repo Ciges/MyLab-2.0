01917bde6a871ec6331458029008b918cc7e708a
José Manuel Ciges Regueiro <jmanuel@ciges.net>
2021-05-31 02:22:22 +0200

    Only add key if it's not already added

--- scripts/server_ssh_keys.sh
+++ scripts/server_ssh_keys.sh
@@ -4,10 +4,17 @@
 users="ciges root"  # Users to configure SSH access
 
 for u in ${users}; do
+    user_dir="$(getent passwd ${u}|cut -f6 -d':')"
     # Add public ssh keys
     ! [[ -d "${user_dir}/.ssh" ]] && mkdir "${user_dir}/.ssh"
     if [[ -d "/tmp/ssh_keys" ]]; then
-        cat /tmp/ssh_keys/* >> "${user_dir}/.ssh/authorized_keys"
-        chown ${u}:${u} "${user_dir}/.ssh/authorized_keys"
+        for kf in /tmp/ssh_keys/*; do
+            # Only add key if it's not already added
+            k="$(cat ${kf}|cut -f2 -d' ')"
+            if ! grep -q "${k}" "${user_dir}/.ssh/authorized_keys"; then
+                echo "Adding public key to user ${u} ..."
+                cat ${kf} >> "${user_dir}/.ssh/authorized_keys"
+            fi;
+        done;
     fi;
 done
