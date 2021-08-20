7834088d52b50619c74b02ea64a9cc4cfeb2641f
José Manuel Ciges Regueiro <jmanuel@ciges.net>
2021-08-20 17:48:12 +0200

    Group wheel and user ciges is created only if they dont exists already

--- scripts/server_init.sh
+++ scripts/server_init.sh
@@ -2,12 +2,15 @@
 
 # Script to initialize recent installed VM's
 
-## add ciges user
-groupadd wheel
-useradd -m ciges -s /bin/bash -G sudo,wheel
-#cp -a /home/vagrant/.ssh /home/ciges/
-chown -R ciges:ciges /home/ciges
-chmod go-rwx /home/ciges
+## add wheel group and ciges user
+if ! grep -q "^wheel:" /etc/group; then
+    groupadd wheel
+fi;
+if ! id ciges &>/dev/null; then 
+    useradd -m ciges -s /bin/bash -G sudo,wheel
+    chown -R ciges:ciges /home/ciges
+    chmod go-rwx /home/ciges
+fi;
 
 ## Update and add packages
 apt-get update
